3e1eeb5bfca18b2d311a85b1b8b2d857e2bbf867
Mateusz Kalisch <mateusz.kalisch@emag.lukasiewicz.gov.pl>
2024-11-25 12:51:22 +0000

    Get params from global mfe config

--- dist/Header.js
+++ dist/Header.js
@@ -9,10 +9,10 @@ import MobileHeader from './MobileHeader';
 import messages from './Header.messages';
 ensureConfig(['LMS_BASE_URL', 'LOGOUT_URL', 'LOGIN_URL', 'SITE_NAME', 'LOGO_URL', 'ORDER_HISTORY_URL', 'SHOW_TELEMETRY_LINKS'], 'Header component');
 subscribe(APP_CONFIG_INITIALIZED, function () {
-  console.log("APP_CONFIG_INITIALIZED, AUTHN_MINIMAL_HEADER:");
-  console.log(process.env.AUTHN_MINIMAL_HEADER);
-  console.log("APP_CONFIG_INITIALIZED, SHOW_TELEMETRY_LINKS:");
-  console.log(process.env.SHOW_TELEMETRY_LINKS);
+  console.log("getConfig().AUTHN_MINIMAL_HEADER:");
+  console.log(getConfig().AUTHN_MINIMAL_HEADER);
+  console.log("getConfig().SHOW_TELEMETRY_LINKS:");
+  console.log(getConfig().SHOW_TELEMETRY_LINKS);
   mergeConfig({
     AUTHN_MINIMAL_HEADER: !!process.env.AUTHN_MINIMAL_HEADER,
     SHOW_TELEMETRY_LINKS: process.env.SHOW_TELEMETRY_LINKS
--- src/Header.jsx
+++ src/Header.jsx
@@ -28,10 +28,10 @@ ensureConfig([
 
 subscribe(APP_CONFIG_INITIALIZED, () => {
 
-  console.log("APP_CONFIG_INITIALIZED, AUTHN_MINIMAL_HEADER:")
-  console.log(process.env.AUTHN_MINIMAL_HEADER)
-  console.log("APP_CONFIG_INITIALIZED, SHOW_TELEMETRY_LINKS:")
-  console.log(process.env.SHOW_TELEMETRY_LINKS)
+  console.log("getConfig().AUTHN_MINIMAL_HEADER:")
+  console.log(getConfig().AUTHN_MINIMAL_HEADER)
+  console.log("getConfig().SHOW_TELEMETRY_LINKS:")
+  console.log(getConfig().SHOW_TELEMETRY_LINKS)
 
   mergeConfig({
     AUTHN_MINIMAL_HEADER: !!process.env.AUTHN_MINIMAL_HEADER,
